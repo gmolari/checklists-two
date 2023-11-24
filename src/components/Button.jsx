e45d3e9f458a3cc5e96dd4cc8a72b4a3a1019c9a
LeoGotard <leonardo.gotardo2@gmail.com>
2023-11-24 15:26:42 -0300

    restarting the project

--- src/components/Button.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function Button({myClass, content, func, data}){
-    return (
-        <button onClick={() => func(data)} className={myClass ? 'default-button '+myClass : 'default-button'}>
-            {content}
-        </button>
-    )
-}
